de9b005ef2cd8eb5aec9fed687d9adc8d855aee8
Remi <remi.pastor@epitech.eu>
2020-02-02 00:38:22 +0100

    Push random background

---
+++ A hole a is a hoole/Assets/Scripts/RandomBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomBackground : MonoBehaviour
+{
+    public Sprite[] backgrounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = Random.Range(0, 3);
+        Debug.Log(num);
+        GetComponent<Image>().sprite = backgrounds[num];
+    }
+}
