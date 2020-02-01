5df9c07d0661c92aa86f9965d9e417b8292cc165
Nicolas <nicolas.guillon@epitech.eu>
2020-02-01 17:10:39 +0100

    MainScene menu with sounds

---
+++ A hole a is a hoole/Assets/MainCanvasScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCanvasScript : MonoBehaviour
+{
+    public Button startButton, optionButton, exitButton;
+    public AudioSource audioData;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(OnStartButton);
+        optionButton.onClick.AddListener(OnOptionButton);
+        exitButton.onClick.AddListener(OnExitButton);
+    }
+    
+    void OnStartButton()
+    {
+        audioData.Play();
+    }
+
+    void OnOptionButton()
+    {
+        audioData.Play();
+    }
+
+    void OnExitButton()
+    {
+        audioData.Play();
+    }
+}
