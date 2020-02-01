eeb431417e4f068af83bb3fd67f544252311bdb3
Remi <remi.pastor@epitech.eu>
2020-02-01 09:21:04 +0100

    Add the script & prefab for the water

---
+++ A hole a is a hoole/Assets/Scripts/WaterMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMove : MonoBehaviour
+{
+    public GameObject waterWall;
+    public float waterVolume = -13;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Vector3 tmp = new Vector3(0, waterVolume, 0);
+        // waterWall.transform.position = tmp;
+        ChangeWater();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeWater();
+    }
+
+    void ChangeWater()
+    {
+        if (waterVolume >= 2)
+        {
+            waterVolume = 2;
+        }
+        else if (waterVolume <= -12.5)
+        {
+            waterVolume = -12.5f;
+        }
+        Vector3 tmp = new Vector3(0, waterVolume, 0);
+        waterWall.transform.position = tmp;
+    }
+}
