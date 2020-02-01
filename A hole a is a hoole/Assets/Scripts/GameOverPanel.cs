28806393e3b4e8bc507fed0cbc624acbbdc2f62f
Remi <remi.pastor@epitech.eu>
2020-02-01 20:43:37 +0100

    Push GameOver Panel

---
+++ A hole a is a hoole/Assets/Scripts/GameOverPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameOverPanel : MonoBehaviour
+{
+    public TextMeshProUGUI TextScoreNumber;
+    public GameObject GameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int _score = (int)GameManager.GetComponent<ScoringManager>().Score;
+        TextScoreNumber.SetText(_score.ToString());
+    }
+
+    void ReturnMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
